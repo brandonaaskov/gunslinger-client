aadb753ff75c3f6baf3f0d3df2133c5e84bb9136
Brandon Aaskov <baaskov@fullscreen.net>
2014-03-24 13:57:09 -0700

    Switching to gulp... because... fuck you grunt.

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -14,19 +14,3 @@
   });
 
 }).call(this);
-
-(function() {
-  angular.module('gunslinger').constant('config', function() {
-    return {
-      zencoder: {
-        integration: '',
-        read: '',
-        full: ''
-      },
-      firebase: {
-        "default": ''
-      }
-    };
-  });
-
-}).call(this);
