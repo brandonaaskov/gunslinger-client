2388ac2f52648d7051d4cc713be94285bfc0268d
Brandon Aaskov <baaskov@fullscreen.net>
2014-03-31 16:41:03 -0700

    Woot! Made this super sexy.

--- scripts/directives/radial.coffee
+++ scripts/directives/radial.coffee
@@ -1,23 +1,39 @@
-angular.module('gunslinger').directive 'radial', () ->
+angular.module('gunslinger').directive 'radial', ($timeout) ->
+  # private vars
+  bgColor = '#e7cfad'
+  color = '#efac4a'
+  canvas = undefined
+  context = undefined
+  lineWidth = undefined
+  radius = undefined
+  xCoord = 0
+  yCoord = 0
+
+
   restrict: 'E'
   replace: true
   templateUrl: '../views/radial.html'
   scope:
     radius: '='
     lineWidth: '='
+    percentComplete: '='
   link: (scope, element) ->
-    bgColor = '#e7cfad'
-    color = '#efac4a'
     canvas = angular.element(element).get(0)
     context = canvas.getContext '2d'
     lineWidth = scope.lineWidth or 20
+    radius = (scope.radius or 100) - (lineWidth)
+    edgeLength = (radius + lineWidth) * 2
+    console.log 'resize canvas to', edgeLength
+    canvas.width = edgeLength
+    canvas.height = edgeLength
     # x/y coordinates are for the center of the circle
     xCoord = scope.radius
     yCoord = scope.radius
-    radius = scope.radius - (lineWidth*2)
-    scope.edgeLength = (scope.radius + lineWidth) * 2
+
+  controller: ($scope) ->
 
     getRadians = (degrees) -> degrees * Math.PI/180
+    getFontSize = -> ($scope.radius * 40)/100 # 40px is 100% for this
 
     # to understand what degrees represent what quadrant of the arc, check out this handy image: http://cl.ly/Ugsu
     drawArc = (startingAngleInDegrees, endingAngleInDegrees, arcColor, lineCap = 'butt') ->
@@ -27,18 +43,22 @@ angular.module('gunslinger').directive 'radial', () ->
       context.lineCap = lineCap
       startAngle = getRadians(startingAngleInDegrees)
       endAngle = getRadians(endingAngleInDegrees)
-      context.arc xCoord, yCoord - lineWidth, radius, startAngle, endAngle
+      context.arc xCoord, yCoord, radius, startAngle, endAngle
       context.stroke()
 
-    addText = (percentComplete, fontSize = '40px', fontFamily = 'Proxima Nova') ->
+    addText = (percentComplete, fontFamily = 'Proxima Nova') ->
       #Lets add the text
       context.fillStyle = color
-      context.font = "#{fontSize} #{fontFamily}"
+      fontSize = getFontSize()
+      context.font = "#{fontSize}px #{fontFamily}"
       context.textAlign = 'center'
-      context.textBaseline = 'bottom'
+      context.textBaseline = 'middle'
       text = percentComplete + '%'
       context.fillText text, xCoord, yCoord
 
-    drawArc -270, 90, bgColor
-    drawArc 90, -210, color
-    addText(15)
+    # if we resize after we've drawn the arc - that's how the Canvas API is designed)
+    $timeout ->
+      drawArc -270, 90, bgColor
+      drawArc 90, -210, color
+      addText 15
+    , 0
--- scripts/directives/radial.js
+++ scripts/directives/radial.js
@@ -1,26 +1,43 @@
 (function() {
-  angular.module('gunslinger').directive('radial', function() {
+  angular.module('gunslinger').directive('radial', function($timeout) {
+    var bgColor, canvas, color, context, lineWidth, radius, xCoord, yCoord;
+    bgColor = '#e7cfad';
+    color = '#efac4a';
+    canvas = void 0;
+    context = void 0;
+    lineWidth = void 0;
+    radius = void 0;
+    xCoord = 0;
+    yCoord = 0;
     return {
       restrict: 'E',
       replace: true,
       templateUrl: '../views/radial.html',
       scope: {
         radius: '=',
-        lineWidth: '='
+        lineWidth: '=',
+        percentComplete: '='
       },
       link: function(scope, element) {
-        var addText, bgColor, canvas, color, context, drawArc, getRadians, lineWidth, radius, xCoord, yCoord;
-        bgColor = '#e7cfad';
-        color = '#efac4a';
+        var edgeLength;
         canvas = angular.element(element).get(0);
         context = canvas.getContext('2d');
         lineWidth = scope.lineWidth || 20;
+        radius = (scope.radius || 100) - lineWidth;
+        edgeLength = (radius + lineWidth) * 2;
+        console.log('resize canvas to', edgeLength);
+        canvas.width = edgeLength;
+        canvas.height = edgeLength;
         xCoord = scope.radius;
-        yCoord = scope.radius;
-        radius = scope.radius - (lineWidth * 2);
-        scope.edgeLength = (scope.radius + lineWidth) * 2;
+        return yCoord = scope.radius;
+      },
+      controller: function($scope) {
+        var addText, drawArc, getFontSize, getRadians;
         getRadians = function(degrees) {
           return degrees * Math.PI / 180;
+        };
+        getFontSize = function() {
+          return ($scope.radius * 40) / 100;
         };
         drawArc = function(startingAngleInDegrees, endingAngleInDegrees, arcColor, lineCap) {
           var endAngle, startAngle;
@@ -33,27 +50,27 @@
           context.lineCap = lineCap;
           startAngle = getRadians(startingAngleInDegrees);
           endAngle = getRadians(endingAngleInDegrees);
-          context.arc(xCoord, yCoord - lineWidth, radius, startAngle, endAngle);
+          context.arc(xCoord, yCoord, radius, startAngle, endAngle);
           return context.stroke();
         };
-        addText = function(percentComplete, fontSize, fontFamily) {
-          var text;
-          if (fontSize == null) {
-            fontSize = '40px';
-          }
+        addText = function(percentComplete, fontFamily) {
+          var fontSize, text;
           if (fontFamily == null) {
             fontFamily = 'Proxima Nova';
           }
           context.fillStyle = color;
-          context.font = "" + fontSize + " " + fontFamily;
+          fontSize = getFontSize();
+          context.font = "" + fontSize + "px " + fontFamily;
           context.textAlign = 'center';
-          context.textBaseline = 'bottom';
+          context.textBaseline = 'middle';
           text = percentComplete + '%';
           return context.fillText(text, xCoord, yCoord);
         };
-        drawArc(-270, 90, bgColor);
-        drawArc(90, -210, color);
-        return addText(15);
+        return $timeout(function() {
+          drawArc(-270, 90, bgColor);
+          drawArc(90, -210, color);
+          return addText(15);
+        }, 0);
       }
     };
   });
